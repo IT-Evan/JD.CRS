46dd55346a8fefe1c263a9955803dd6036daaee2
IT-Evan <Evan.net@163.com>
2019-10-05 09:56:28 +0800

    AddPaged

--- src/JD.CRS.Application/Paged/PagedSortedAndFilteredInputDto.cs
+++ src/JD.CRS.Application/Paged/PagedSortedAndFilteredInputDto.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace JD.CRS.Common
+namespace JD.CRS.Paged
 {
     /// <summary>
     /// PagedSortedAndFilteredInputDto
