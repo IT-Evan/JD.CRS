e1b6f4e7c279768d3b9a5570822a984a5788d7ad
IT-Evan <Evan.net@163.com>
2019-10-07 10:55:22 +0800

    AddPaged

--- src/JD.CRS.Web.Mvc/wwwroot/view-resources/Views/Course/Index.js
+++ src/JD.CRS.Web.Mvc/wwwroot/view-resources/Views/Course/Index.js
@@ -1,88 +1,66 @@
-﻿(function () {
-    $(function () {
-
-        var _courseService = abp.services.app.course;
-        var _$modal = $('#CourseCreateModal');
-        var _$form = _$modal.find('form');
-
-        _$form.validate({
-        });
-
-        $('#RefreshButton').click(function () {
-            refreshCourseList();
-        });
-
-        $('.delete-course').click(function () {
-            var courseId = $(this).attr("data-course-id");
-            var courseName = $(this).attr('data-course-name');
-            deleteCourse(courseId, courseName);
-        });
-
-
-        $('.edit-course').click(function (e) {
-            var courseId = $(this).attr("data-course-id");
-
-            e.preventDefault();
-            $.ajax({
-                url: abp.appPath + 'Course/EditCourseModal?courseId=' + courseId,
-
-                type: 'POST',
-                contentType: 'application/html',
-                success: function (content) {
-                    $('#CourseEditModal div.modal-content').html(content);
-                },
-
-                error: function (e) { }
-            });
-        });
-
-        _$form.find('button[type="submit"]').click(function (e) {
-            e.preventDefault();
-
-            if (!_$form.valid()) {
-                return;
-            }
-
-            var course = _$form.serializeFormToObject(); //serializeFormToObject is defined in main.js         
-
-            abp.ui.setBusy(_$modal);
-            _courseService.create(course).done(function () {
-                _$modal.modal('hide');
-                location.reload(true); //reload page to see new user!
-
-            }).always(function () {
-                abp.ui.clearBusy(_$modal);
-            });
-        });
-
-
-        _$modal.on('shown.bs.modal', function () {
-            _$modal.find('input:not([type=hidden]):first').focus();
-
-        });
-
-
-        function refreshCourseList() {
-            location.reload(true); //reload page to see new user!
-
+﻿$(function () {
+    //define var
+    var _courseService = abp.services.app.course;
+    var _$modal = $('#CourseCreateModal');
+    var _$form = _$modal.find('form[name=CourseCreateForm]');
+    //save for create
+    function save() {
+        if (!_$form.valid()) {
+            return;
         }
-
-        function deleteCourse(courseId, courseName) {
-            abp.message.confirm(
-                abp.utils.formatString(abp.localization.localize('AreYouSureWantToDelete', 'CRS'), courseName),
-
-                function (isConfirmed) {
-                    if (isConfirmed) {
-                        _courseService.delete({
-                            id: courseId
-
-                        }).done(function () {
-                            refreshCourseList();
-
-                        });
-                    }
-                }
-            );
-        }
+        var course = _$form.serializeFormToObject(); //serializeFormToObject is defined in main.js   
+        abp.ui.setBusy(_$form); //loading-begin
+        _courseService.create(course).done(function () {
+            _$modal.modal('hide');
+            location.reload(true); //reload page
+        }).always(function () {
+            abp.ui.clearBusy(_$modal); //loading-end
+        });
+    }
+    _$form.validate({});
+    $('#RefreshButton').click(function () {
+        refreshCourseList();
     });
-})();
+    $('.delete-course').click(function () {
+        var courseId = $(this).attr("data-course-id");
+        var courseName = $(this).attr('data-course-name');
+        deleteCourse(courseId, courseName);
+    });
+    $('.edit-course').click(function (e) {
+        var courseId = $(this).attr("data-course-id");
+        e.preventDefault();
+        $.ajax({
+            url: abp.appPath + 'Course/EditCourseModal?courseId=' + courseId,
+            type: 'POST',
+            contentType: 'application/html',
+            success: function (content) {
+                $('#CourseEditModal div.modal-content').html(content);
+            },
+            error: function (e) { }
+        });
+    });
+    _$form.find('button[type="submit"]').click(function (e) {
+        e.preventDefault();
+        save();        
+    });
+    _$modal.on('shown.bs.modal', function () {
+        _$modal.find('input:not([type=hidden]):first').focus();
+    });    
+    function refreshCourseList() {
+        location.reload(true); //reload page to see new user!
+    }
+    function deleteCourse(courseId, courseName) {
+        abp.message.confirm(
+            abp.utils.formatString(abp.localization.localize('AreYouSureWantToDelete', 'CRS'), courseName),
+            function (isConfirmed) {
+                if (isConfirmed) {
+                    _courseService.delete({
+                        id: courseId
+                    }).done(function () {
+                        refreshCourseList();
+                    });
+                }
+            }
+        );
+    }
+});
--- src/JD.CRS.Web.Mvc/wwwroot/view-resources/Views/Course/_EditCourseModal.js
+++ src/JD.CRS.Web.Mvc/wwwroot/view-resources/Views/Course/_EditCourseModal.js
@@ -1,52 +1,30 @@
-﻿(function ($) {
+﻿$(function () {
+    //define var
     var _courseService = abp.services.app.course;
     var _$modal = $('#CourseEditModal');
     var _$form = $('form[name=CourseEditForm]');
-
-
+    //save for create
     function save() {
-
         if (!_$form.valid()) {
             return;
         }
-
         var course = _$form.serializeFormToObject(); //serializeFormToObject is defined in main.js   
-
-
-        abp.ui.setBusy(_$form);
+        abp.ui.setBusy(_$form); //loading-begin
         _courseService.update(course).done(function () {
-
             _$modal.modal('hide');
-
-            location.reload(true); //reload page to see edited course!
+            location.reload(true); //reload page
         }).always(function () {
-            abp.ui.clearBusy(_$modal);
+            abp.ui.clearBusy(_$modal); //loading-end
         });
     }
-
-
     //Handle save button click
     _$form.closest('div.modal-content').find(".save-button").click(function (e) {
         e.preventDefault();
         save();
-
     });
-
-    //Handle enter key
-    _$form.find('input').on('keypress', function (e) {
-
-        if (e.which === 13) {
-            e.preventDefault();
-            save();
-        }
-
-    });
-
-    $.AdminBSB.input.activate(_$form);
-
+    //$.AdminBSB.input.activate(_$form);
     _$modal.on('shown.bs.modal', function () {
         _$form.find('input[type=text]:first').focus();
 
     });
-
-})(jQuery);
+});
