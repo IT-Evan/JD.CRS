4fb3711fc0fc0f95e22a0da1b001eea60461693f
IT-Evan <Evan.net@163.com>
2019-10-09 21:13:58 +0800

    AddSearchByStatus

--- src/JD.CRS.Application/Course/Dto/GetAllCoursesInput.cs
+++ src/JD.CRS.Application/Course/Dto/GetAllCoursesInput.cs
@@ -1,13 +1,9 @@
-﻿using JD.CRS.Entitys;
-using JD.CRS.Paged;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Abp.Application.Services.Dto;
+﻿using Abp.Application.Services.Dto;
+using JD.CRS.Entitys;
 
 namespace JD.CRS.Course.Dto
 {
-    public class GetAllCoursesInput//: PagedResultRequestDto //PagedSortedAndFilteredInputDto
+    public class GetAllCoursesInput: PagedResultRequestDto //PagedSortedAndFilteredInputDto
     {
         public StatusCode? Status { get; set; }
     }
