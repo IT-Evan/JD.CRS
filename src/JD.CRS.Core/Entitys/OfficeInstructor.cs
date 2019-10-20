ef2263dad80705be8203addb147dc1cb956e6494
IT-Evan <Evan.net@163.com>
2019-10-20 10:57:45 +0800

    AddOperationEntity

---
+++ src/JD.CRS.Core/Entitys/OfficeInstructor.cs
@@ -0,0 +1,66 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JD.CRS.Entitys
+{
+    public class OfficeInstructor : Entity<int>, IHasCreationTime
+    {
+        public OfficeInstructor()
+        {
+            this.OfficeCode = string.Empty;
+            this.InstructorCode = string.Empty;
+            this.Remarks = string.Empty;
+            this.Status = StatusCode.Enabled;
+            this.CreateDate = null;
+            this.CreateName = string.Empty;
+            this.UpdateDate = null;
+            this.UpdateName = string.Empty;
+            this.CreationTime = Clock.Now;
+        }
+
+        /// <summary>
+        /// 办公室编号
+        /// </summary>
+        [StringLength(50)]
+        public string OfficeCode { get; set; }
+        /// <summary>
+        /// 教职员编号
+        /// </summary>
+        [StringLength(50)]
+        public string InstructorCode { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [StringLength(200)]
+        public string Remarks { get; set; }
+        /// <summary>
+        /// 状态: 0 有效, 1 无效
+        /// </summary>
+        public StatusCode Status { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        [StringLength(50)]
+        public string CreateName { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        [StringLength(50)]
+        public string UpdateName { get; set; }
+
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ src/JD.CRS.Core/Entitys/StudentCourse.cs
@@ -0,0 +1,66 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JD.CRS.Entitys
+{
+    public class StudentCourse : Entity<int>, IHasCreationTime
+    {
+        public StudentCourse()
+        {
+            this.StudentCode = string.Empty;
+            this.CourseCode = string.Empty;
+            this.Remarks = string.Empty;
+            this.Status = StatusCode.Enabled;
+            this.CreateDate = null;
+            this.CreateName = string.Empty;
+            this.UpdateDate = null;
+            this.UpdateName = string.Empty;
+            this.CreationTime = Clock.Now;
+        }
+
+        /// <summary>
+        /// 学生编号
+        /// </summary>
+        [StringLength(50)]
+        public string StudentCode { get; set; }
+        /// <summary>
+        /// 课程编号
+        /// </summary>
+        [StringLength(50)]
+        public string CourseCode { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [StringLength(200)]
+        public string Remarks { get; set; }
+        /// <summary>
+        /// 状态: 0 正常, 1 废弃
+        /// </summary>
+        public StatusCode Status { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        [StringLength(50)]
+        public string CreateName { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+        /// <summary>
+        /// 修改人
+        /// </summary>
+        [StringLength(50)]
+        public string UpdateName { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+    }
+}
