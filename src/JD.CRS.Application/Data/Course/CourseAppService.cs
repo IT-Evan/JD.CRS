21d856b662b765b89e6a9b83c802254ade4d97e1
IT-Evan <Evan.net@163.com>
2020-09-10 09:24:11 +0800

    Clear

--- src/JD.CRS.Application/Data/Course/CourseAppService.cs
+++ src/JD.CRS.Application/Data/Course/CourseAppService.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace JD.CRS.Course
 {
-    public class CourseAppService : AsyncCrudAppService<Entitys.Course, CourseReadDto, int, PagedResultRequestDto, //GetAllCoursesInput,
+    public class CourseAppService : AsyncCrudAppService<Entitys.Course, CourseReadDto, int, PagedResultRequestDto,//GetAllCoursesInput,
                              CourseWriteDto, CourseWriteDto>, ICourseAppService
 
     {
@@ -28,7 +28,7 @@ namespace JD.CRS.Course
             return base.Create(input);
         }
 
-        public override async Task<PagedResultDto<CourseReadDto>> GetAll(PagedResultRequestDto input) //(GetAllCoursesInput input)
+        public override async Task<PagedResultDto<CourseReadDto>> GetAll(PagedResultRequestDto input)//(GetAllCoursesInput input)
         {
             //组合查询(此服务端查询功能已废弃，由Datatables客户端查询替代)
             //var query = base.CreateFilteredQuery(input)
